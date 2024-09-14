8befe4dfd59b5acddc268f38a36f3ac3d8d01903
Anna Babayan <ababayan@coastal.edu>
2024-09-14 15:35:03 -0400

    Implemented Auth file for Role Access

--- backend/app/core/auth.py
+++ backend/app/core/auth.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 from jose import jwt
 from datetime import datetime, timedelta
 from passlib.context import CryptContext
@@ -14,10 +15,14 @@ def verify_password(plain_password, hashed_password):
 def create_access_token(data: dict, role: str):
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({"exp": expire, "role": role})  # Include user's role in the token
+    to_encode.update({"exp": expire, "role": role})  # Included user's role in the token
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
+# decode and verify jwt token
+# args - token (str) - the access token to decode. 
+# returns the decoded token if valid
+# raises HTTPException if token is invalid
 def decode_jwt(token: str):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
